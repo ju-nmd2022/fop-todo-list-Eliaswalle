56e60cfd0f1a54c2bcf7c3e0a91220deadbe79f3
Eliaswalle <wael22nq@student.ju.se>
2023-04-11 17:05:34 +0200

    local

--- java.js
+++ java.js
@@ -8,9 +8,18 @@ addButton.addEventListener("click", function () {
 function inputElement() {
   let inputField = document.getElementById("input-to-do");
   const taskField = inputField.value;
+  // taskArray.push(taskField);
+  if (localStorage.taskField === undefined) {
+    localStorage.taskField = JSON.stringify(taskArray);
+  }
+  let taskArray = JSON.parse(localStorage.taskField);
   taskArray.push(taskField);
+  localStorage.taskField = JSON.stringify(taskArray);
+
+  console.log(taskArray);
+  console.log(localStorage);
 }
-console.log();
+
 // function inputElement() {
 //   const listOfTask = document.getElementById("input-to-do");
 //   listOfTask.innerText = taskArray;
