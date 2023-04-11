7069042268873bee8217ea50126084adfb14e784
Eliaswalle <wael22nq@student.ju.se>
2023-04-11 17:45:54 +0200

    big changes

--- java.js
+++ java.js
@@ -1,25 +1,50 @@
 let taskArray = [];
+const listElement = document.getElementById("listOfTask");
 
+if (localStorage.taskField) {
+  // localStorage.taskField = JSON.stringify(taskArray);
+  taskArray = JSON.parse(localStorage.taskField);
+}
 const addButton = document.getElementById("button-add-list");
 addButton.addEventListener("click", function () {
   inputElement();
+  taskLoop();
 });
 
 function inputElement() {
   let inputField = document.getElementById("input-to-do");
   const taskField = inputField.value;
   // taskArray.push(taskField);
-  if (localStorage.taskField === undefined) {
-    localStorage.taskField = JSON.stringify(taskArray);
-  }
-  let taskArray = JSON.parse(localStorage.taskField);
+
   taskArray.push(taskField);
   localStorage.taskField = JSON.stringify(taskArray);
-
-  console.log(taskArray);
   console.log(localStorage);
+  console.log(taskArray);
 }
 
+function taskLoop() {
+  listElement.innerHTML = "";
+  for (let task of taskArray) {
+    const taskElement = document.createElement("div");
+    taskElement.innerText = task;
+    listElement.appendChild(taskElement);
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    listElement.appendChild(removeButton);
+
+    const index = taskArray.indexOf(task);
+
+    removeButton.addEventListener("click", () => {
+      taskArray.splice(index, 1);
+      taskLoop();
+    });
+
+    const completeButton = document.createElement("button");
+    completeButton.innerText
+  }
+}
+taskLoop();
 // function inputElement() {
 //   const listOfTask = document.getElementById("input-to-do");
 //   listOfTask.innerText = taskArray;
