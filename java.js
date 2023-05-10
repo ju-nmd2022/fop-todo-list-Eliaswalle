f3ef3ebcb678b916741163a8e2c7f4529985dcfa
Eliaswalle <wael22nq@student.ju.se>
2023-05-10 16:26:43 +0200

    update to object

--- java.js
+++ java.js
@@ -1,21 +1,27 @@
 let taskArray = [];
 const listElement = document.getElementById("listOfTask");
 
-if (localStorage.taskField) {
-  taskArray = JSON.parse(localStorage.taskField);
+if (localStorage.taskObject) {
+  taskArray = JSON.parse(localStorage.taskObject);
 }
 const addButton = document.getElementById("button-add-list");
 addButton.addEventListener("click", function () {
   inputElement();
   taskLoop();
 });
-
 function inputElement() {
   let inputField = document.getElementById("input-to-do");
-  const taskField = inputField.value;
+  // const taskField = inputField.value;
 
-  taskArray.push(taskField);
-  localStorage.taskField = JSON.stringify(taskArray);
+  taskObject = {
+    value: inputField.value,
+    complete: false,
+  };
+  console.log(taskObject);
+
+  taskArray.push(taskObject);
+  // taskArray.push(taskField);
+  localStorage.taskObject = JSON.stringify(taskArray);
 }
 
 function taskLoop() {
@@ -23,7 +29,7 @@ function taskLoop() {
   for (let task of taskArray) {
     const taskElement = document.createElement("div");
     taskElement.classList.add("taskDiv");
-    taskElement.innerText = task;
+    taskElement.innerText = task.value;
     listElement.appendChild(taskElement);
 
     const removeButton = document.createElement("button");
@@ -35,6 +41,8 @@ function taskLoop() {
 
     removeButton.addEventListener("click", () => {
       taskArray.splice(index, 1);
+
+      localStorage.taskObject = JSON.stringify(taskArray);
       taskLoop();
     });
 
@@ -51,22 +59,27 @@ function taskLoop() {
     // with this code below i used the help off chatGPT to save my "line-through" with local storage!
     //The code from 43-51 was the code that i did not get help from chat to get reference on what i got help with!!
     //Also got help from a classmate to solve some issues on displaying the array on the screen using a for loop.
-
     const completeButton = document.createElement("button");
     completeButton.innerText = "complete";
     taskElement.appendChild(completeButton);
     completeButton.classList.add("complete");
 
     // Check if the task has been completed before and update the style if necessary
-    if (localStorage.getItem(taskElement.innerText)) {
+
+    // if (localStorage.getItem(taskElement.innerText)) {
+    //   taskElement.style.textDecoration = "line-through";
+    // }
+    if (localStorage.getItem(task.complete === true)) {
       taskElement.style.textDecoration = "line-through";
     }
 
     completeButton.addEventListener("click", function () {
       taskElement.style.textDecoration = "line-through";
+      task.complete = true;
 
       // Save the completion state to local storage
-      localStorage.setItem(taskElement.innerText, "completed");
+      //  localStorage.setItem(taskElement.innerText, "completed");
+      localStorage.taskObject = JSON.stringify(taskArray);
     });
   }
 }
